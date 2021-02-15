099e391048424f165bbb944ab24e1f5f628ff46c
IFallowed <1178885073@qq.com>
2021-02-15 11:59:22 +0800

    IU-2020.3.2 <11788@LAPTOP-8R3O7D6C Create debugger.xml, lombok-plugin.xml, gitee.xml, colors.scheme.xml, databaseDrivers.xml, updates.xml, Enum.java, Interface.java, rainbow_brackets.xml, editor.xml, ide.general.xml, project.default.xml, baseRefactoring.xml, shared-indexes.xml, wsl.distributions.xml, databaseSettings.xml, pluginAdvertiser.xml, KeyPromoterXSettings.xml, path.macros.xml, sonarlint.xml, toStringTemplates.xml, laf.xml, ide-features-trainer.xml, Default.xml, codota.xml, GrepConsole.xml, Class.java, jdk.table.xml, lightEdit.xml, filetypes.xml, vcs.xml, mybatis.xml, applicationLibraries.xml, javarebel.xml, templates.xml, MethodAnnotation.xml, find.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @Description:
+ * @Author: IFallowed
+ * @Date: ${DATE} ${TIME}
+ * @Version: 1.0
+ */
+public interface ${NAME} {
+}
